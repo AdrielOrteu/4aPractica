e25b67e64cdc906a7f1d9d2112a39944aa1ca058
Saul <1752970@uab.cat>
2024-12-16 17:04:02 +0100

    dia2

---
+++ my_tee.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(const char argc int *argv[]){
+
+int main(const char
+if (fd == -1)
+
+else
+  
+  }
+
+
+ssize_t read(int fd, void argc, size_t *argv[])
+
+ssize_t write(int fd, void argc, size_t count)
+
+
+
+

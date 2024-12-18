e8969a49ec3310b56e3f715a2d5149288e92c841
Adriel <1750927@uab.cat>
2024-12-18 22:57:51 +0100

    Adriel finished 2nd-EXERCICE git status!  (ADRIEL IS ME (btw) )

--- my_cmp.c
+++ my_cmp.c
@@ -14,22 +14,22 @@ int main( int argc, char *argv[] ){
     close(fd2);
     perror("my_cmp");
   }else{
-    printf("%d %d\n", fd1, fd2);
-    // loop structure that returns a way to identify characters [non_keyword]:
+
     int byte_num = 1;
-    int line_num = 1:
+    int line_num = 1;
     char file1_ch;
     char file2_ch;
-    ssize_t_read(fd1, file1_ch, 1);
-    ssize_t_read(fd2, file2_ch, 1);
-    while (byte_num < file_size && file1_ch == file2_ch) {
-      ssize_t_read(fd1, file1_ch, 1);
-      ssize_t_read(fd2, file2_ch, 1);
-      if (non_keyword == "\n"){
+    read(fd1, &file1_ch, 1);
+    read(fd2, &file2_ch, 1);
+
+    while (file1_ch != 0 && file1_ch == file2_ch) {
+      if (file1_ch == '\n'){
         line_num ++;
-      }else{
-        bytes ++;
       }
+      byte_num ++;
+      read(fd1, &file1_ch, 1);
+      read(fd2, &file2_ch, 1);
     }
+    printf("%s %s differ: byte %d, line %d\n", argv[1], argv[2], byte_num, line_num);
   }
 }
